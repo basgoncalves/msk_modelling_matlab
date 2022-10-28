d78217c2ad5da6abf9c081999bf3b12a8bfa3f1c
unknown <Bas>
2022-10-28 18:19:27 +0200

    add InspectEMG_bops

--- src/bops_tool/src/functions/FAI_BG/OrganizingScrips/findFolders.m
+++ src/bops_tool/src/functions/FAI_BG/OrganizingScrips/findFolders.m
@@ -1,9 +1,9 @@
 % find trials 
 
-function TrialList_out = findFolders(Path,TrialList)
+function TrialList_out = findFolders(Path,TrialList,CannotContain)
 
 fp = filesep;
 files = dir ([Path]);
 names = {files.name};
-idx = find(contains(names,TrialList,'IgnoreCase',true)& ~contains (names,'0'));
+idx = find(contains(names,TrialList,'IgnoreCase',true)& ~contains (names,CannotContain));
 TrialList_out =  names(idx);
