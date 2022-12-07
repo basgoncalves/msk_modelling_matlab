7203f5bc18484f14719a032f130821baf99c9f4e
basgoncalves <basilio.goncalves7@gmail.com>
2022-12-07 11:54:07 +0100

    make activate_msk_modelling refer to itself instead of the current open function

--- activate_msk_modelling.m
+++ activate_msk_modelling.m
@@ -2,8 +2,8 @@
 % by Basilio Goncalves, basilio.goncalves7@gmail.com, https://github.com/basgoncalves
 function activate_msk_modelling
 clear; clc; close all;                                                                                              % clean workspace (use restoredefaultpath if needed)
-activeFile = matlab.desktop.editor.getActive;                                                                       % get dir of the current file
-msk_dir  = fileparts(activeFile.Filename);                                                                          
+activeFile = [mfilename('fullpath') '.m'];                                                                          % get dir of the current file
+msk_dir  = fileparts(activeFile);                                                                          
 addpath(genpath(msk_dir));                                                                                          % add current folder to MATLAB path
 
 disp([msk_dir ' activated'])
