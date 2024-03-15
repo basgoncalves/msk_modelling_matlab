8f58c5037758cf75bb5a1b9bf2759bfaa26db266
basgoncalves <basilio.goncalves7@gmail.com>
2024-03-15 12:20:25 +0100

    delete stats SPM python

--- src/Stats/spm1d_python/spm1d/data/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from . import uv0d, mv0d
-from . import uv1d, mv1d
-
-
-
-
-
--- src/Stats/spm1d_python/spm1d/data/mv0d/__init__.py
+++
@@ -1,5 +0,0 @@
-
-
-from . import cca
-from . import hotellings1, hotellings2, hotellings_paired
-from . import manova1
